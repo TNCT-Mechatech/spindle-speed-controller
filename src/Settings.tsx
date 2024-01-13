3e0dee1c6be3be377ea8868fcce507048224e703
testusuke <testusuke@gmail.com>
2024-01-13 23:01:13 +0900

    feat: implement setting page

---
+++ src/Settings.tsx
@@ -0,0 +1,12 @@
+import {Stack} from "@mui/material";
+import MaxSpeedSettings from "./assets/MaxSpeedSettings.tsx";
+
+export default function Settings() {
+    return (
+        <>
+            <Stack spacing={2}>
+                <MaxSpeedSettings/>
+            </Stack>
+        </>
+    )
+}
