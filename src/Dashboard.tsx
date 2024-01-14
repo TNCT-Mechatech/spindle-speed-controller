ac5d8af05380bb88c280a0804dd695cc8556b2e7
testusuke <testusuke@gmail.com>
2024-01-14 16:11:31 +0900

    feat: serial port selector

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,12 +1,16 @@
 import {Box, Divider, Grid, Stack} from "@mui/material";
 import TargetSetting from "./TargetSetting.tsx";
 import Operation from "./Operation.tsx";
+import SerialPortSelector from "./SerialPortSelector.tsx";
 
 export default function Dashboard() {
     return (
         <Box mx={"10px"} my={"10px"}>
             <Grid container width={"100%"} spacing={1}>
                 <Grid item width={"50%"}>
+                    <Stack spacing={1}>
+                        <SerialPortSelector/>
+                    </Stack>
                 </Grid>
                 <Grid item width={"50%"}>
                     <Stack spacing={1}>
